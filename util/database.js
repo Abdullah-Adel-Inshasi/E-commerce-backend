e78ebd813b873dfd9ff8634d80f99fb4c68fb7d8
Abdullah Adel <abdullah.adel.inshasi@gmail.com>
2022-12-26 11:24:51 +0200

    test

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
 const URI = `mongodb+srv://${process.env.MONGO_CLIENT_USERNAME}:${process.env.MONGO_CLIENT_PASSWORD}@cluster0.oxsvm3u.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(URI);
-// 
+// test commit
 module.exports.mongoClient = client;
