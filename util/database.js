850e388539372520ba188d93c934bd33eb939eb5
Abdullah Adel <abdullah.adel.inshasi@gmail.com>
2022-12-27 20:38:46 +0200

    create mongoDB connection

--- util/database.js
+++ util/database.js
@@ -1,5 +1,21 @@
 const { MongoClient } = require("mongodb");
-const URI = `mongodb+srv://${process.env.MONGO_CLIENT_USERNAME}:${process.env.MONGO_CLIENT_PASSWORD}@cluster0.oxsvm3u.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(URI);
+const URI = `mongodb+srv://${process.env.MONGO_CLIENT_USERNAME}:${process.env.MONGO_CLIENT_PASSWORD}@cluster0.oxsvm3u.mongodb.net/shop?retryWrites=true&w=majority`;
+let _db;
+const mongoConnect = (callback) => {
+  MongoClient.connect(URI)
+    .then((client) => {
+      callback();
+      _db = client.db();
+    })
+    .catch(console.error);
+};
+
+const getDB = () => {
+  if (!_db) {
+    throw new Error("No Database found");
+  }
+
+  return _db;
+};
 // test commit
-module.exports.mongoClient = client;
+module.exports = { mongoConnect, getDB };
