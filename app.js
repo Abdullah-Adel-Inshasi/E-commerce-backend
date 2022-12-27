be0bd560ffe1c558a65625495986a6d0f0fce7bb
Abdullah Adel <abdullah.adel.inshasi@gmail.com>
2022-12-27 20:39:29 +0200

    use mongo connection to start the app

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-require('dotenv').config()
+require("dotenv").config();
 const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { mongoClient } = require("./util/database");
+const { mongoConnect } = require("./util/database");
 
 const errorController = require("./controllers/error");
 // Models
-// const { User } = require("./models/user");
+const { User } = require("./models/user");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, _res, next) => {
-//   User.findByPk(1)
-//     .then((user) => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(console.error);
-// });
+app.use((req, _res, next) => {
+  User.findById("63a9b291643feba55a679fc3")
+    .then((mongoUser) => {
+      const user = new User({
+        id: mongoUser._id,
+        cart: mongoUser.cart,
+        email: mongoUser.email,
+        name: mongoUser.name,
+      });
+      req.user = user;
+      next();
+    })
+    .catch(console.error);
+});
 
-// app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 // app.use(errorController.get404);
-console.log(
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.MONGO_CLIENT_PASSWORD}@cluster0.oxsvm3u.mongodb.net/?retryWrites=true&w=majority`
-);
-mongoClient
-  .connect()
-  .then((client) => {
-    console.log(client);
-    app.listen(3000);
-  })
-  .catch(console.error);
+mongoConnect(() => {
+  app.listen(3000);
+});
